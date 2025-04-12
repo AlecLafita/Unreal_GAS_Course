8fb7f599fc31caf7bbbd081a67a8e709f6b3f3f2
Alec Lafita Secanilla <alec.lafita@gmail.com>
2025-04-12 14:47:01 +0200

    Added apply/remove effects in code and make fire area use it

--- Source/Aura/Private/Actor/EffectActor.cpp
+++ Source/Aura/Private/Actor/EffectActor.cpp
@@ -23,13 +23,69 @@ void AEffectActor::BeginPlay()
 
 void AEffectActor::ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass)
 {
-	if (UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor))
-	{
-		check(GameplayEffectClass);
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (!IsValid(TargetAbilitySystemComponent))
+		return;
+
+	check(GameplayEffectClass);
 		
-		FGameplayEffectContextHandle EffectContextHandle = TargetAbilitySystemComponent->MakeEffectContext();
-		EffectContextHandle.AddSourceObject(this);
-		const FGameplayEffectSpecHandle EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
-		TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+	FGameplayEffectContextHandle EffectContextHandle = TargetAbilitySystemComponent->MakeEffectContext();
+	EffectContextHandle.AddSourceObject(this);
+	const FGameplayEffectSpecHandle EffectSpecHandle = TargetAbilitySystemComponent->MakeOutgoingSpec(GameplayEffectClass, 1.f, EffectContextHandle);
+	const FActiveGameplayEffectHandle ActiveEffectHandle = TargetAbilitySystemComponent->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+
+	const bool bIsInfinite = EffectSpecHandle.Data.Get()->Def.Get()->DurationPolicy == EGameplayEffectDurationType::Infinite; //We're assuming policy doesn't change at runtime
+	if (bIsInfinite && InfiniteGameplayEffectRemovalPolicy != EEffectRemovalPolicy::DoNotRemove)
+	{
+		const uint32 UID = TargetActor->GetUniqueID();;
+		ActiveInfiniteEffects.Add(UID, ActiveEffectHandle);
 	}
+}
+
+/* TODO if different effectActors have overlapping areas and we have a stack < the amount of actors overlapping, it can
+ * happen that when exiting the overlapping area and all stacks are removed but we're still inside one of the actors
+ * area, no effect will be applied!
+ */
+void AEffectActor::OnBeginOverlap(AActor* TargetActor)
+{
+	if (InstantGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+
+	if (DurationGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+
+	if (InfiniteGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnOverlap)
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+}
+
+void AEffectActor::OnEndOverlap(AActor* TargetActor)
+{
+	if (InstantGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		ApplyEffectToTarget(TargetActor, InstantGameplayEffectClass);
+
+	if (DurationGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		ApplyEffectToTarget(TargetActor, DurationGameplayEffectClass);
+
+	if (InfiniteGameplayEffectApplicationPolicy == EEffectApplicationPolicy::ApplyOnEndOverlap)
+		ApplyEffectToTarget(TargetActor, InfiniteGameplayEffectClass);
+
+	if (InfiniteGameplayEffectRemovalPolicy == EEffectRemovalPolicy::RemovalOnEndOverlap)
+		RemoveInfiniteEffect(TargetActor);
+}
+
+void AEffectActor::RemoveInfiniteEffect(AActor* TargetActor)
+{
+	UAbilitySystemComponent* TargetAbilitySystemComponent = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(TargetActor);
+	if (!IsValid(TargetAbilitySystemComponent))
+		return;
+		
+	const uint32 UID = TargetActor->GetUniqueID();
+	if (!ActiveInfiniteEffects.Contains(UID))
+		return;
+ 
+	const FActiveGameplayEffectHandle& EffectHandle = ActiveInfiniteEffects[UID];
+	if (!TargetAbilitySystemComponent->RemoveActiveGameplayEffect(EffectHandle, 1))
+		return;
+ 
+	ActiveInfiniteEffects.Remove(UID);
 }
--- Source/Aura/Public/Actor/EffectActor.h
+++ Source/Aura/Public/Actor/EffectActor.h
@@ -3,10 +3,27 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "ActiveGameplayEffectHandle.h"
 #include "GameFramework/Actor.h"
 #include "EffectActor.generated.h"
 
 class UGameplayEffect;
+class UAbilitySystemComponent;
+
+UENUM(BlueprintType)
+enum class EEffectApplicationPolicy : uint8
+{
+	ApplyOnOverlap,
+	ApplyOnEndOverlap,
+	DoNotApply
+};
+
+UENUM(BlueprintType)
+enum class EEffectRemovalPolicy : uint8
+{
+	RemovalOnEndOverlap,
+	DoNotRemove
+};
 
 UCLASS()
 class AURA_API AEffectActor : public AActor
@@ -20,11 +37,42 @@ protected:
 	virtual void BeginPlay() override;
 
 	UFUNCTION(BlueprintCallable)
-	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass); 
-	
+	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+	UFUNCTION(BlueprintCallable)
+	void OnBeginOverlap(AActor* TargetActor);
+
+	UFUNCTION(BlueprintCallable)
+	void OnEndOverlap(AActor* TargetActor);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	bool bDestroyOnEffectRemoval = false;
+
+	//TODO have an array of effects
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	EEffectApplicationPolicy InstantGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
 	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	EEffectApplicationPolicy DurationGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	TSubclassOf<UGameplayEffect> InfiniteGameplayEffectClass;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	EEffectApplicationPolicy InfiniteGameplayEffectApplicationPolicy = EEffectApplicationPolicy::DoNotApply;;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Effects")
+	EEffectRemovalPolicy InfiniteGameplayEffectRemovalPolicy = EEffectRemovalPolicy::DoNotRemove;;
+
+private:
+
+	void RemoveInfiniteEffect(AActor* TargetActor);
+	
+	TMap<uint32, FActiveGameplayEffectHandle> ActiveInfiniteEffects;
 };
