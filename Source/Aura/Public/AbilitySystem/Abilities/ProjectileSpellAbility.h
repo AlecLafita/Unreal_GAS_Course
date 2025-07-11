3a30a1ceb6d207984efc4b17e797f9e39cc35b44
Alec Lafita Secanilla <alec.lafita@gmail.com>
2025-07-11 19:18:45 +0200

    WIP created skeleton for spell ability and added to chr initial abilities

---
+++ Source/Aura/Public/AbilitySystem/Abilities/ProjectileSpellAbility.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/BaseGameplayAbility.h"
+#include "ProjectileSpellAbility.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UProjectileSpellAbility : public UBaseGameplayAbility
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
