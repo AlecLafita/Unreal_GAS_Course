b1d48caef6809b7c1f8aac4356e0fdf0ebe46b82
Alec Lafita Secanilla <alec.lafita@gmail.com>
2025-07-17 20:29:00 +0200

    Added new task that outputs mouse click position, and added debug sphere there. TODO server replication

---
+++ Source/Aura/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
+
+UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
+{
+	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
+	return MyObj;
+}
+
+void UTargetDataUnderMouse::Activate()
+{
+	const APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
+	check(PC);
+	
+	FHitResult CursorHit;
+	PC->GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+
+	ValidData.Broadcast(CursorHit.Location);
+}
